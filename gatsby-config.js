f4227460c26164178e0d715b220949ad6a56b79d
mahsa-nhp <60693758+mahsa-nhp@users.noreply.github.com>
2021-01-10 10:20:58 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,15 @@
+// configuratie van gatsby
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    // title hier aanpassen => SEO
+    title: `The World Of Film`,
+    // een description van ons project
+    description: ``,
+    author: `Mahsa Nasir Hajipour`,
   },
+  // alle plugins definieren van gatsby
   plugins: [
+    // voor SEO
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -13,6 +18,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    // image optimalisation
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -27,6 +33,26 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      // Gatsby Source GraphQL
+      // Met de gatsby source graphQL plugin zullen we onze Headless WordPress GraphQL API integreren met het GraphQL schema van Gatsby.
+      resolve: "gatsby-source-graphql",
+      options: {
+      typeName: "WPGraphql",
+      fieldName: "wpcontent",
+      url: "http://the-world-of-film.local//graphql", // de link naar de route van je website
+      },
+      },
+      {
+        // Gatsby Plugin Google Fonts
+        resolve: `gatsby-plugin-google-fonts`,
+        options: {      
+        fonts: [`Roboto`, `Oswald`],      
+        display: "swap",        
+        },        
+        },
+        "gatsby-plugin-styled-components", //!!!
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
