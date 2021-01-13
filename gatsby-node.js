797660e50a5b8759a8cb60ba547e1aac0a0c3283
mahsa-nhp <60693758+mahsa-nhp@users.noreply.github.com>
2021-01-13 13:16:04 +0100

    dynamic film Pages toegevoegd

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,41 +2,42 @@ const { graphql } = require("gatsby")
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 const path = require("path")
 
-// exports.createPages = ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   return graphql(`
-//     {
-//       wpcontent {
-//         films {
-//           edges {
-//             node {
-//               slug
-//               id
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `).then(result => {
-//     if (result.errors) {
-//       result.errors.forEach(e => console.error(e.toString()))
-//       return Promise.reject(result.errors)
-//     }
+// dynamish pagina 
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      wpcontent {
+        films {
+          edges {
+            node {
+              slug
+              id
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
 
-//     const films = result.data.wpcontent.films.edges
-//     films.forEach(film => {
-//       const { id, slug } = film.node
-//       createPage({
-//         path: slug,
-//         component: path.resolve(`src/templates/film.js`),
-//         context: {
-//           id,
-//           slug,
-//         },
-//       })
-//     })
-//   })
-// }
+    const films = result.data.wpcontent.films.edges
+    films.forEach(film => {
+      const { id, slug } = film.node
+      createPage({
+        path: slug,
+        component: path.resolve(`src/templates/film.js`),
+        context: {
+          id,
+          slug,
+        },
+      })
+    })
+  })
+}
 
 /* Aan de hand van dit stukje code worden de images vanuit WPgraphql omgezet tot images waarop Gatsby image optimization kan toepassen */
 exports.createResolvers = async ({
---
+++ src/Templates/film.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const FilmTemplate = () => {
+    console.log('OK');
+    return <div>Hello</div>
+}
+
+export default FilmTemplate
